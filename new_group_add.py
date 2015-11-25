7f37269bba01630bb465f9a5ea95a83bfe643bd6
aalekperov <alekperov.atash@gmail.com>
2015-11-25 14:13:36 +0300

    Тест не запускается. Проблема с pytest

--- new_group_add.py
+++ new_group_add.py
@@ -1,37 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 from application_group import Application_Group
-import unittest
+import pytest
 from group import Group
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class new_group_add(unittest.TestCase):
-    def setUp(self):
-        self.app = Application_Group()
+@pytest.fixture
+def app(request):
+    fixture = Application_Group()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def test_new_group_add(self):
-
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="New group", header="New group header", footer="New group footer"))
-        self.app.logout()
-
-    def test_new_empty_group_add(self):
-
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+def test_new_group_add(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="New group", header="New group header", footer="New group footer"))
+    app.logout()
 
 
-    def tearDown(self):
-        self.app.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_new_empty_group_add(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
