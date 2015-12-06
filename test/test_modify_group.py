cdfacdbe087978e620be83b2414312eaaea85fd7
aalekperov <alekperov.atash@gmail.com>
2015-12-06 03:23:34 +0300

    Первая проверка валидности для группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,5 +5,5 @@ from model.group import Group
 
 def test_modify_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="Modified group", header="Modified group header", footer="Modified group footer"))
+    app.group.modify(Group(name="Modified group"))
     app.session.logout()
