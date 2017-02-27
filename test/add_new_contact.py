a0ba43f14cb9e5827aeb242f1a275479bde2314a
aalekperov <alekperov.atash@gmail.com>
2017-02-27 14:17:03 +0300

    Home is defined as home phone number

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -6,12 +6,12 @@ def test_add_new_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname = "Homer", middlename = "Jay", lastname = "Simpson", nickname = "simpson",
              photo_path = 'c:\\GitHub\\Task1\\res\\homer.jpg', title = "worker",
-             company = "Springfield Nuclear Power Plant", company_address = "Springfield", home = "Springfield, 742 Evergreen Terrace",
+             company = "Springfield Nuclear Power Plant", company_address = "Springfield", home = "+1(123)456-67-00",
              mobile_phone_num = "+1(123)456-67-89", work_phone_num = "+1(123)456-67-89", fax_num = "+1(123)456-67-89",
              email1 = "homer.simpson@fox.tv", email2 = "homer.simpson@fox.tv", email3 = "", homepage = "http://simpsons.com",
              birthday_d = "12", birthday_m = "May", birthday_y = "1959",
              anniversary_d = "10", anniversary_m = "May", anniversary_y = "1957",
-             second_address = "Springfield", second_home = "742 Evergreen Terrace", notes = "No comments")
+             second_address = "Springfield", second_home = "+1(123)456-67-05", notes = "No comments")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
