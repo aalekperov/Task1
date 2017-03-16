73cf9a731ad892879d38591cf69d0dab7aa1914a
aalekperov <alekperov.atash@gmail.com>
2017-03-16 19:40:59 +0300

    Генерация тестовых данных для групп

--- test/new_group_add.py
+++ test/new_group_add.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*20
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 30))
-    for i in range(5)
-    #Group(name=name, header=header, footer=footer)
-    #for name in ["", random_string("name", 10)]
-    #for header in ["", random_string("header", 20)]
-    #for footer in ["", random_string("footer", 30)]
-]
+from data.add_group import constant as testdata
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
